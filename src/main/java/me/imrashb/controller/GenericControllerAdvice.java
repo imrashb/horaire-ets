5d33665bcbc9ef0d3d45ffe3310feacc8cfb2273
ImRashB <45215136+imrashb@users.noreply.github.com>
2022-11-03 16:24:01 -0400

    handle missing parameter

---
+++ src/main/java/me/imrashb/controller/GenericControllerAdvice.java
@@ -0,0 +1,18 @@
+package me.imrashb.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GenericControllerAdvice {
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity handleMissingParams(MissingServletRequestParameterException ex) {
+        String name = ex.getParameterName();
+        return new ResponseEntity("Missing parameter '"+name+"'. Type: '"+ex.getParameterType()+"'", HttpStatus.BAD_REQUEST);
+    }
+
+}
