e0f073df26e2afd5efc8ca2caecf2f3fe803009c
ImRashB <45215136+imrashb@users.noreply.github.com>
2022-11-14 20:36:36 -0500

    tool pour faire des themes

--- src/main/java/me/imrashb/TestImageHoraire.java
+++ src/main/java/me/imrashb/TestImageHoraire.java
@@ -2,16 +2,20 @@ package me.imrashb;
 
 import me.imrashb.domain.*;
 import me.imrashb.utils.*;
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
 
 import javax.imageio.*;
 import javax.swing.*;
+import javax.swing.colorchooser.*;
+import javax.swing.event.*;
 import java.awt.*;
+import java.awt.datatransfer.*;
+import java.awt.event.*;
 import java.awt.image.*;
+import java.beans.*;
 import java.io.*;
 import java.util.*;
 import java.util.List;
+import java.util.function.*;
 
 public class TestImageHoraire {
 
@@ -67,17 +71,99 @@ public class TestImageHoraire {
 
         CombinaisonHoraire comb = new CombinaisonHoraire(groupes);
 
-        Image image = new HoraireImageMaker(comb, HoraireImageMaker.DARK_THEME).drawHoraire();
+        HoraireImageMakerTheme theme = HoraireImageMaker.LIGHT_THEME;
+        Image image = new HoraireImageMaker(comb, theme).drawHoraire();
 
         ImageIO.write((RenderedImage) image, "png", new File("here.png"));
+
+        List<ButtonConsumer> buttons = new ArrayList<>();
+
 
         Image resultingImage = image.getScaledInstance((int) (image.getWidth(null)/2f), (int) (image.getHeight(null)/2f), Image.SCALE_DEFAULT);
         JLabel picLabel = new JLabel(new ImageIcon(resultingImage));
         JFrame frame = new JFrame();
         frame.add(picLabel);
-        frame.setSize(new Dimension(1400, 900));
-        frame.setPreferredSize(new Dimension(1400, 900));
+        frame.setLayout(new GridLayout(1,2));
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        panel.setLayout(new GridLayout(buttons.size(), 1));
+
+        ActionListener listener = event -> {
+            System.out.println("HERE");
+            Image img = new HoraireImageMaker(comb, theme).drawHoraire();
+            try {
+                ImageIO.write((RenderedImage) img, "jpeg", new File("save.jpeg"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(theme.getColorBackground());
+            Image result = img.getScaledInstance((int) (img.getWidth(null)/2f), (int) (img.getHeight(null)/2f), Image.SCALE_DEFAULT);
+            picLabel.setIcon(new ImageIcon(result));
+            picLabel.repaint();
+        };
+
+        buttons.add(new ButtonConsumer("Background",theme::setColorBackground, listener));
+        buttons.add(new ButtonConsumer("Jour",theme::setColorJour, listener));
+        buttons.add(new ButtonConsumer("Heure",theme::setColorHeure, listener));
+        buttons.add(new ButtonConsumer("Ligne separation ",theme::setColorLigneSeparation, listener));
+        buttons.add(new ButtonConsumer("Dash Ligne Separation",theme::setColorDashedLigneSeparation, listener));
+        buttons.add(new ButtonConsumer("Fin de semaine",theme::setColorFinDeSemaine, listener));
+        buttons.add(new ButtonConsumer("Texte cours",theme::setColorTexteCours, listener));
+        buttons.add(new ButtonConsumer("Texte outline",theme::setColorTexteOutline, listener));
+        JButton generate = new JButton("Generate");
+        generate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("public static final HoraireImageMakerTheme THEME_NAME = new HoraireImageMakerTheme(\n" +
+                        "            \"Thème NAME\",Color.decode(\""+hex(theme.getColorBackground())+"\"), Color.decode(\""+hex(theme.getColorLigneSeparation())+"\"), Color.decode(\""+hex(theme.getColorDashedLigneSeparation())+"\"), Color.decode(\""+hex(theme.getColorFinDeSemaine())+"\"), Color.decode(\""+hex(theme.getColorTexteCours())+"\"), Color.decode(\""+hex(theme.getColorTexteOutline())+"\"), Color.decode(\""+hex(theme.getColorJour())+"\"), Color.decode(\""+hex(theme.getColorHeure())+"\"));");
+            }
+        });
+        for(ButtonConsumer but : buttons) {
+            panel.add(but);
+            panel.setPreferredSize(new Dimension(200,10000));
+        }
+        panel.add(generate);
+
+        frame.setSize(new Dimension(1700, 900));
+        frame.setPreferredSize(new Dimension(1700, 900));
+        frame.validate();
+        frame.pack();
         frame.setVisible(true);
+    }
+
+    public static String hex(Color color) {
+        return String.format("#%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
+    }
+
+    public static final HoraireImageMakerTheme THEME_NAME = new HoraireImageMakerTheme(
+            "Thème NAME",Color.decode("#001408"), Color.decode("#006635"), Color.decode("#ff0ad6"), Color.decode("#003d1d"), Color.decode("#ffffff"), Color.decode("#000000"), Color.decode("#ff0ad6"), Color.decode("#ff0ad6"));
+
+}
+
+class ButtonConsumer extends JButton {
+
+    private Consumer<Color> consumer;
+
+    public ButtonConsumer(String label, Consumer<Color> consumer, ActionListener listener) {
+        this.setText(label);
+        this.consumer = consumer;
+        this.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String data = null;
+                try {
+                    data = (String) Toolkit.getDefaultToolkit()
+                            .getSystemClipboard().getData(DataFlavor.stringFlavor);
+                    consumer.accept(Color.decode("#"+data));
+
+                    listener.actionPerformed(null);
+                } catch (UnsupportedFlavorException ex) {
+                    throw new RuntimeException(ex);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
     }
 
 }
--- src/main/java/me/imrashb/utils/HoraireImageMaker.java
+++ src/main/java/me/imrashb/utils/HoraireImageMaker.java
@@ -15,13 +15,21 @@ public class HoraireImageMaker {
     public static final HoraireImageMakerTheme LIGHT_THEME = new HoraireImageMakerTheme(
             "Thème lumière",Color.white, Color.gray, Color.gray, Color.lightGray, Color.white, Color.black, Color.gray, Color.gray);
     public static final HoraireImageMakerTheme DARK_THEME = new HoraireImageMakerTheme(
-            "Thème sombre",Color.darkGray, Color.lightGray, Color.lightGray, Color.gray, Color.black, Color.white, Color.white, Color.white);
+            "Thème sombre",Color.darkGray, Color.lightGray, Color.lightGray, Color.gray, Color.white, Color.black, Color.white, Color.white);
     public static final HoraireImageMakerTheme CYAN_THEME = new HoraireImageMakerTheme(
             "Thème cyan",Color.decode("#023047"),Color.decode("#8ECAE6"), Color.decode("#FFB703"),
-            Color.decode("#219EBC"), Color.black, Color.white, Color.decode("#FB8500"), Color.decode("#FB8500"));
+            Color.decode("#219EBC"), Color.white, Color.black, Color.decode("#FB8500"), Color.decode("#FB8500"));
     public static final HoraireImageMakerTheme FOREST_THEME = new HoraireImageMakerTheme(
             "Thème forêt",Color.decode("#283618"),Color.decode("#FEFAE0"), Color.decode("#DDA15E"),
-            Color.decode("#606C38"), Color.black, Color.white, Color.decode("#DDA15E"),Color.decode("#DDA15E"));
+            Color.decode("#606C38"), Color.white, Color.black, Color.decode("#DDA15E"),Color.decode("#DDA15E"));
+    public static final HoraireImageMakerTheme NIGHT_THEME = new HoraireImageMakerTheme(
+            "Thème nuit",Color.decode("#000814"), Color.decode("#003566"), Color.decode("#ffc300"), Color.decode("#001d3d"), Color.decode("#ffffff"), Color.decode("#000000"), Color.decode("#ffd60a"), Color.decode("#ffd60a"));
+    public static final HoraireImageMakerTheme PURPLE_THEME = new HoraireImageMakerTheme(
+            "Thème mauve",Color.decode("#240046"), Color.decode("#5a189a"), Color.decode("#7b2cbf"), Color.decode("#3c096c"), Color.decode("#ffffff"), Color.decode("#000000"), Color.decode("#c77dff"), Color.decode("#c77dff"));
+    public static final HoraireImageMakerTheme BLUE_THEME = new HoraireImageMakerTheme(
+            "Thème bleu",Color.decode("#012a4a"), Color.decode("#61a5c2"), Color.decode("#2a6f97"), Color.decode("#013a63"), Color.decode("#ffffff"), Color.decode("#000000"), Color.decode("#89c2d9"), Color.decode("#89c2d9"));
+
+
     public static final java.util.List<HoraireImageMakerTheme> themes;
 
     private static final Font COURS_FONT;
@@ -34,6 +42,9 @@ public class HoraireImageMaker {
         themes.add(DARK_THEME);
         themes.add(CYAN_THEME);
         themes.add(FOREST_THEME);
+        themes.add(NIGHT_THEME);
+        themes.add(PURPLE_THEME);
+        themes.add(BLUE_THEME);
 
         final String filename = "/bahnschrift.ttf";
         InputStream is = HoraireImageMaker.class.getResourceAsStream(filename);
